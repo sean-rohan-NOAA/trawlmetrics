101134f9bbefcf0d43b10b0af42945fe64382531
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-10-10 21:02:15 -0700

    Initial commit

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' Create a database connection using RODBC
+#'
+#' A function that accepts a data source name, username, and password to establish returns an Oracle DataBase Connection (ODBC) as an RODBC class in R.
+#'
+#' @param schema Data source name (DSN) as a character vector.
+#' @return An RODBC class ODBC connection.
+#' @noRd
+
+get_connected <- function(channel = NULL, schema = NA){
+  if(is.null(channel)) {
+    (echo = FALSE)
+    if(is.na(schema)) {
+      schema <- getPass::getPass(msg = "Enter ORACLE schema: ")
+    }
+    username <- getPass::getPass(msg = "Enter your ORACLE Username: ")
+    password <- getPass::getPass(msg = "Enter your ORACLE Password: ")
+    channel  <- RODBC::odbcConnect(dsn = paste(schema),
+                                   uid = paste(username),
+                                   pwd = paste(password),
+                                   believeNRows = FALSE)
+  }
+  return(channel)
+}
